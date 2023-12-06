fd86037fdaee813367f428a6ba1afea8856e0116
DescaMr <drizal07@gmail.com>
2023-12-06 20:42:18 +0700

    add icon calendar, watch & money post_bottom_bar and add model

--- lib/components/post_bottom_bar.dart
+++ lib/components/post_bottom_bar.dart
@@ -5,10 +5,11 @@ class PostBottomBar extends StatelessWidget {
   final Beach place;
 
   const PostBottomBar({Key? key, required this.place}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height/2,
+      height: MediaQuery.of(context).size.height / 2,
       padding: EdgeInsets.only(top: 20, left: 20, right: 20),
       decoration: BoxDecoration(
         color: Color(0xFFEDF2F6),
@@ -50,63 +51,70 @@ class PostBottomBar extends StatelessWidget {
                   ],
                 ),
                 SizedBox(height: 25),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_today,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          place.openDay,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.access_time,
+                          color: Colors.green,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          place.openTime,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.attach_money,
+                          color: Colors.orange,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          place.pay,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
                 Text(
                   place.description,
                   style: TextStyle(color: Colors.black54, fontSize: 16),
-                textAlign: TextAlign.justify,
+                  textAlign: TextAlign.justify,
                 ),
                 SizedBox(height: 20),
-                // Row(
-                //   children: [
-                //     Padding( 
-                //     padding: EdgeInsets.only(right: 5),
-                //     child: Expanded(
-                //       child: ClipRRect(
-                //         borderRadius: BorderRadius.circular(15),
-                //         child: Image.asset(
-                //           "images/city5.jpg",
-                //           fit: BoxFit.cover,
-                //           width: 120,
-                //           height: 90,
-                //         ),
-                //       ),
-                //     ),
-                //     ),
-                //     Padding( 
-                //     padding: EdgeInsets.only(right: 5),
-                //     child: Expanded(
-                //       child: ClipRRect(
-                //         borderRadius: BorderRadius.circular(15),
-                //         child: Image.asset(
-                //           "images/city4.jpg",
-                //           fit: BoxFit.cover,
-                //           width: 120,
-                //           height: 90,
-                //         ),
-                //       ),
-                //     ),
-                //     ),
-                //     Expanded(
-                //       child: Container(
-                //         alignment: Alignment.center,
-                //         width: 120,
-                //         height: 90,
-                //         margin: EdgeInsets.only(right: 5),
-                //         decoration: BoxDecoration(
-                //           color: Colors.black,
-                //           borderRadius: BorderRadius.circular(15),
-                //           image: DecorationImage(
-                //             image: AssetImage("images/city6.jpg"),
-                //             fit: BoxFit.cover,
-                //              opacity: 0.4,
-                //           ),
-                //         ),
-                //       ), 
-                //     ),
-                //   ],
-                // ),
               ],
-            ), 
+            ),
           ),
         ],
       ),
--- lib/pages/dummy.data.dart
+++ lib/pages/dummy.data.dart
@@ -5,6 +5,7 @@ class Beach {
   String rating;
   String openDay;
   String openTime;
+  String pay;
   String imageAsset;
 
   Beach({
@@ -14,6 +15,7 @@ class Beach {
     required this.rating,
     required this.openDay,
     required this.openTime,
+    required this.pay,
     required this.imageAsset,
   });
 }
@@ -24,8 +26,9 @@ Beach(
   Location: 'Cianjur',
    description: 'Wisata Karang Potong Cianjur terletak di Kampung Sedekan, Desa Saganten, Kecamatan Sindangbarang, Kabupaten Cianjur, Jawa Barat. Jam operasionalnya mulai pukul 08:00 hingga 21:00 WIB Keindahan Karang Potong Cianjur ini terletak pada bangunan-bangunan berwarna terang, yaitu perpaduan putih dan biru dengan design layaknya istana fantasi. Tidak cukup sampai di situ, perpaduan warna pelangi akan Anda temukan di sini. Dalam bangunannya, disisipi oleh rumput-rumput hijau segar dan pohon kelapa khas pantai. Wisata Ocean View memiliki fasilitas untuk anak-anak hingga dewasa. Anak-anak akan menikmati berbagai perosotan, sepeda mini yang lucu, ayunan, bermain pasir, dan lainnya.',
    rating: '4.9',
-    openDay: 'open everyday',
-     openTime: 'openTime',
+    openDay: 'Setiap Hari',
+     openTime: '08.00 - 21.00 WIB',
+     pay: '15.000,-',
       imageAsset: 'images/city1.jpg'),
 
 Beach(
@@ -33,17 +36,19 @@ Beach(
   Location: 'Cianjur',
    description: 'Merupakan tempat objek wisata sekaligus pelabuhan tradisional.Di Pantai ini kita bisa menyewa perahu untuk ketengah laut. Fasilitas penunjang seperti tempat makan, saung - saung untuk berteduh dan bisa menginap di penginapan - penginapan dengan harga terjangkau. Harga masuk pelabuhan Jayanti untuk roda dua berkisar 5.000,- Sedangkan untuk roda empat 15.000,-',
    rating: '4.6',
-    openDay: 'open everyday',
-     openTime: 'openTime',
+    openDay: 'Setiap Hari',
+     openTime: '09.00 - 22.00 WIB',
+     pay: '15.000,-',
       imageAsset: 'images/city2.jpg'),
 
 Beach(
   name: 'Apra Beach',
   Location: 'Cianjur',
-   description: 'Pantai Apra berlokasi di Kecamatan Sindangbrang, Cianjur Selatan atau lebih tepatnya sekitar 300 meter dari Alun-alun Sindangbarang. Biasanya, pantai ini ramai dikunjungi saat liburan panjang dan lebaran tiba. Dinamakan Pantai Apra, ternyata juga mengandung sejarah yang cukup penting.Pantai Apra Sindangbarang masih dikelola swadaya sehinggaharga tiket masuknyapun tidak mahal. Hanya berkisar dari Rp 2.000 –  Rp 5.000 ',
+   description: 'Pantai Apra berlokasi di Kecamatan Sindangbarang, Cianjur Selatan atau lebih tepatnya sekitar 300 meter dari Alun-alun Sindangbarang. Biasanya, pantai ini ramai dikunjungi saat liburan panjang dan lebaran tiba. Dinamakan Pantai Apra, ternyata juga mengandung sejarah yang cukup penting.Pantai Apra Sindangbarang masih dikelola swadaya sehinggaharga tiket masuknyapun tidak mahal. Hanya berkisar dari Rp 2.000 –  Rp 5.000 ',
    rating: '4.7',
-    openDay: 'open everyday',
-     openTime: 'openTime',
+    openDay: 'Setiap Hari',
+     openTime: '08.00 - 23.00 WIB',
+     pay: '5.000,-',
       imageAsset: 'images/city3.jpg'),
 ];
  
